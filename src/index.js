5fb8b7fd9f5add555237866aa20b15698d71f717
Amit Kumar <amitkumr1404@gmail.com>
2024-04-03 15:27:14 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Style.css';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.querySelector('.container-fluid'));
 root.render(
-  <>
-  <header>Header</header>
-  <main>
-    <div className='container h-100 a_cont'>Main</div>
-  </main>
-  <footer>Footer</footer>
-  </>
+ <App></App>
 );
